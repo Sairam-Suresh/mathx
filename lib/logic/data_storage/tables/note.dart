795011c483ba68a972dd5dfd52134b746f3a8852
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-16 16:04:22 +0800

    Created custom row class for notes table

--- lib/logic/data_storage/tables/note.dart
+++ lib/logic/data_storage/tables/note.dart
@@ -7,3 +7,31 @@ class Notes extends Table {
   DateTimeColumn get lastModifiedDate => dateTime()();
   BoolColumn get renderMath => boolean()();
 }
+
+// Custom row class for the Notes table
+class Note {
+  final String id;
+  final String name;
+  final String content;
+  final DateTime lastModifiedDate;
+  final bool renderMath;
+
+  Note(
+      {required this.id,
+      required this.name,
+      required this.content,
+      required this.lastModifiedDate,
+      required this.renderMath});
+
+  factory Note.fromDeepLink(Uri uri) {
+    var decodedData = utf8.decode(base64Decode(uri.queryParameters["source"]!));
+    var extractedData = decodedData.split(" ␢␆␝⎠⎡⍰⎀ ");
+
+    return Note(
+        name: extractedData[0],
+        content: extractedData[1].replaceAll("~", "~~"),
+        renderMath: extractedData[2] == "true" ? true : false,
+        lastModifiedDate: DateTime.now(),
+        id: const Uuid().v1());
+  }
+}
