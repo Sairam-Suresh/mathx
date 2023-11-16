6307bd6c58628b51b9364626aeadac72b1e07178
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-16 15:20:35 +0800

    Created database instance, adding tables later on

---
+++ lib/logic/data_storage/database.dart
@@ -0,0 +1,29 @@
+// These imports are necessary to open the sqlite3 database
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:mathx/logic/data_storage/tables/note.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase(tables: [Notes])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase.createInBackground(file);
+  });
+}
