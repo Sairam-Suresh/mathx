c7232230761420c9667175eb46a3225eefdc1924
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-27 19:34:43 +0800

    Added more scaffolding for NoteEditor

--- lib/screens/notes/note_editor.dart
+++ lib/screens/notes/note_editor.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mathx/logic/custom_hooks/note_hook.dart';
+import 'package:mathx/logic/data_storage/tables/math_notes.dart';
+import 'package:mathx/logic/riverpods/notes_riverpod.dart';
 
-class NoteEditor extends StatelessWidget {
+class NoteEditor extends HookConsumerWidget {
   const NoteEditor({
     super.key,
     required this.uuid,
@@ -10,18 +15,75 @@ class NoteEditor extends StatelessWidget {
   final String uuid;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-                onPressed: () {
-                  context.pop();
-                },
-                icon: const Icon(Icons.check))
-          ],
-        ),
-        body: Text(uuid));
+  Widget build(BuildContext context, WidgetRef ref) {
+    var note = useGetNote(ref, uuid);
+    var titleController = useTextEditingController();
+    var titleFocusNode = useFocusNode();
+
+    var newTitle = useState("");
+    var newContent = useState("");
+    var newRenderMath = useState(false);
+
+    var newNote = useState<MathNote?>(null);
+
+    useEffect(() {
+      // Initialise variables with data from the database
+      if (note != null) {
+        titleController.text = note.name;
+        newTitle.value = note.name;
+        newContent.value = note.content;
+        newRenderMath.value = note.renderMath;
+      }
+      return null;
+    }, [note]);
+
+    useEffect(() {
+      newNote.value = MathNote(
+          uuid: uuid,
+          name: newTitle.value,
+          content: newContent.value,
+          lastModifiedDate: DateTime.now(),
+          renderMath: newRenderMath.value);
+
+      return null;
+    }, [newTitle.value, newContent.value, newRenderMath.value]);
+
+    return (note != null)
+        ? Scaffold(
+            appBar: AppBar(
+              title: IntrinsicWidth(
+                child: EditableText(
+                  backgroundCursorColor: Theme.of(context).primaryColor,
+                  onChanged: (newText) {
+                    newTitle.value = newText;
+                  },
+                  controller: titleController,
+                  focusNode: titleFocusNode,
+                  style: TextStyle(
+                      fontSize:
+                          Theme.of(context).textTheme.titleLarge?.fontSize),
+                  cursorColor: Theme.of(context).brightness == Brightness.dark
+                      ? Colors.white
+                      : Colors.black,
+                ),
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      if (newNote.value != null) {
+                        ref
+                            .read(notesRiverpodProvider.notifier)
+                            .updateNoteEntryInDb(newNote.value!);
+                      }
+                      context.pop();
+                    },
+                    icon: const Icon(Icons.check))
+              ],
+            ),
+            body: Text(uuid))
+        : const Scaffold(
+            body: Center(
+            child: CircularProgressIndicator(),
+          ));
   }
 }
