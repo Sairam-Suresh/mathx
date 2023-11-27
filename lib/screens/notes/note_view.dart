9f5af61673b67402acdb4bd7148fde7f164a044c
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-27 16:46:00 +0800

    Renamed note_view_and_editor.dart to note_view.dart

---
+++ lib/screens/notes/note_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mathx/logic/data_storage/tables/note.dart';
+import 'package:mathx/logic/riverpods/notes_riverpod.dart';
+
+class NoteView extends HookConsumerWidget {
+  const NoteView({super.key, required this.uuid});
+
+  final String uuid;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var note = useState<Note?>(null);
+
+    useEffect(() {
+      (ref.read(notesRiverpodProvider.notifier).obtainNoteByUUID(uuid))
+          .then((value) {
+        note.value = value;
+      });
+
+      return null;
+    }, []);
+
+    return Scaffold(
+        appBar: (note.value != null)
+            ? AppBar(
+                title: Text(note.value!.name),
+              )
+            : null,
+        body: (note.value != null)
+            ? const Center(
+                child: Text("Amongus"),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ));
+  }
+}
