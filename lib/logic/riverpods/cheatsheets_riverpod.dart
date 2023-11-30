45240c0818784426edcb654bbadab3ae0ea36be7
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-11-30 22:16:02 +0800

    Implemented helper methods in riverpod to deal with cheatsheets

--- lib/logic/riverpods/cheatsheets_riverpod.dart
+++ lib/logic/riverpods/cheatsheets_riverpod.dart
@@ -1,5 +1,8 @@
+import 'package:drift/drift.dart';
+import 'package:mathx/logic/data_storage/database.dart';
 import 'package:mathx/logic/data_storage/tables/cheatsheets.dart';
 import 'package:mathx/logic/riverpods/db_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'cheatsheets_riverpod.g.dart';
@@ -11,5 +14,32 @@ class CheatsheetsRiverpod extends _$CheatsheetsRiverpod {
     var db = ref.read(databaseRiverpodProvider);
 
     return db.select(db.cheatsheets).get();
+  }
+
+  // Cheatsheet itself, followed by a link to it.
+  Future<(Cheatsheet, String)> obtainCheatsheetByName(String name) async {
+    var db = ref.read(databaseRiverpodProvider);
+
+    var targetSheet = await ((db.select(db.cheatsheets)
+          ..where((tbl) => tbl.name.equals(name)))
+        .getSingle());
+
+    var pathToSheet =
+        "${(await getApplicationDocumentsDirectory()).path}/cheatsheets/pdfs/${targetSheet.name}.pdf";
+
+    return (targetSheet, pathToSheet);
+  }
+
+  Future changeCheatsheetLikeStatus(String noteName) async {
+    var db = ref.read(databaseRiverpodProvider);
+
+    var targetSheet = await ((db.select(db.cheatsheets)
+          ..where((tbl) => tbl.name.equals(noteName)))
+        .getSingle());
+
+    (db.update(db.cheatsheets)..where((tbl) => tbl.name.equals(noteName)))
+        .write(CheatsheetsCompanion(starred: Value(!targetSheet.starred)));
+
+    ref.invalidateSelf();
   }
 }
