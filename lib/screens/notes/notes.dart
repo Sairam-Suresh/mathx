94b100ed8ad61df7263524300f9fddcb804bf69a
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-12-01 14:32:00 +0800

    Refactored ListView and showed a message if search term didnt match any notes

--- lib/screens/notes/notes.dart
+++ lib/screens/notes/notes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mathx/logic/data_storage/tables/math_notes.dart';
 import 'package:mathx/logic/riverpods/notes_riverpod.dart';
 import 'package:mathx/screens/notes/deeplinked_note_error_dialogs.dart';
 import 'package:mathx/widgets/note_list_tile.dart';
@@ -14,6 +15,8 @@ class Notes extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var searchTerm = useState("");
     var notes = ref.watch(notesRiverpodProvider);
+
+    var searchController = useTextEditingController();
 
     if (failedToObtainNote) {
       Future.delayed(
@@ -48,6 +51,17 @@ class Notes extends HookConsumerWidget {
                       padding: EdgeInsets.only(left: 8.0),
                       child: Icon(Icons.search),
                     ),
+                    controller: searchController,
+                    trailing: (searchTerm.value.isNotEmpty)
+                        ? [
+                            IconButton(
+                                onPressed: () {
+                                  searchController.clear();
+                                  searchTerm.value = "";
+                                },
+                                icon: const Icon(Icons.clear))
+                          ]
+                        : null,
                     onChanged: (newText) {
                       searchTerm.value = newText;
                     },
@@ -58,15 +72,58 @@ class Notes extends HookConsumerWidget {
             ),
             Expanded(
                 child: switch (notes) {
-              AsyncData(:final value) => ListView(
-                  children: value.map((e) => NoteListTile(note: e)).toList(),
-                ),
+              AsyncData(:final value) =>
+                buildNoteListView(value, searchTerm, context),
               AsyncError(:final error) => Text(error.toString()),
               _ => const Center(child: CircularProgressIndicator()),
             })
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildNoteListView(List<MathNote> mathNotes,
+      ValueNotifier<String> searchTerm, BuildContext context) {
+    final mathNotesCopy = mathNotes.map((e) => e).toList();
+
+    if (searchTerm.value.isNotEmpty) {
+      mathNotesCopy.removeWhere((element) => !(element.name
+          .toLowerCase()
+          .contains(searchTerm.value.toLowerCase())));
+    }
+    if (mathNotesCopy.isEmpty) {
+      return Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(),
+              Text(
+                "We could not find any notes with your search term",
+                style: Theme.of(context).textTheme.headlineSmall,
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                "Try using another search term",
+                textAlign: TextAlign.center,
+              ),
+              const Spacer()
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: mathNotesCopy
+          .map((e) =>
+              (e.name.toLowerCase().contains(searchTerm.value.toLowerCase()))
+                  ? NoteListTile(note: e)
+                  : Container())
+          .toList(),
     );
   }
 }
