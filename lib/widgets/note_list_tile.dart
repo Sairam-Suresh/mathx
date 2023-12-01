49a43d6598a8498b1716e630f7e06a41a257d5c3
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-12-01 22:23:52 +0800

    Added slide-able actions to note list tile, like delete and share

--- lib/widgets/note_list_tile.dart
+++ lib/widgets/note_list_tile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mathx/logic/data_storage/tables/math_notes.dart';
+import 'package:mathx/logic/riverpods/notes_riverpod.dart';
+import 'package:mathx/widgets/share_note_sheet.dart';
 
 class NoteListTile extends ConsumerWidget {
   const NoteListTile({super.key, required this.note});
@@ -10,16 +13,67 @@ class NoteListTile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListTile(
-      title: Text(
-        note.name,
-        style: const TextStyle(fontWeight: FontWeight.bold),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                    title: const Text("Warning"),
+                    content: Text(
+                        "Are you sure you want to delete the note \"${note.name}\"?"),
+                    actions: [
+                      FilledButton(
+                          onPressed: () {
+                            ref
+                                .read(notesRiverpodProvider.notifier)
+                                .deleteNoteFromDb(note);
+                            context.pop();
+                          },
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.red),
+                              foregroundColor:
+                                  MaterialStateProperty.all(Colors.white)),
+                          child: const Text("Yes")),
+                      FilledButton(
+                          child: const Text("Cancel"),
+                          onPressed: () {
+                            context.pop();
+                          })
+                    ]),
+              );
+            },
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            onPressed: (context) async {
+              await shareNote(context, note);
+            },
+            backgroundColor: Colors.blueAccent,
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ],
       ),
-      subtitle: Text(note.lastModifiedDate.toLocal().toString()),
-      trailing: const Icon(Icons.chevron_right),
-      onTap: () {
-        context.go("/notes/view/${note.uuid}");
-      },
+      child: ListTile(
+        title: Text(
+          note.name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(note.lastModifiedDate.toLocal().toString()),
+        trailing: const Icon(Icons.chevron_right),
+        onTap: () {
+          context.go("/notes/view/${note.uuid}");
+        },
+      ),
     );
   }
 }
