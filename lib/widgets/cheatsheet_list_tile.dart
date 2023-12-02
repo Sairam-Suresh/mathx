f502f6c2c05659d6ddb825bca01bd69bd5063412
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-12-02 08:56:30 +0800

    Added slide-able action to toggle cheatsheet like status

--- lib/widgets/cheatsheet_list_tile.dart
+++ lib/widgets/cheatsheet_list_tile.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mathx/logic/data_storage/tables/cheatsheets.dart';
+import 'package:mathx/logic/riverpods/cheatsheets_riverpod.dart';
 
 Widget cheatsheetIcon(int level) {
   if (level == 1) {
@@ -24,20 +28,48 @@ Widget cheatsheetIcon(int level) {
   }
 }
 
-class CheatsheetListTile extends StatelessWidget {
+class CheatsheetListTile extends HookConsumerWidget {
   const CheatsheetListTile({super.key, required this.sheet});
 
   final Cheatsheet sheet;
 
   @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: cheatsheetIcon(sheet.secondaryLevel),
-      title: Text(sheet.name),
-      trailing: const Icon(Icons.chevron_right),
-      onTap: () {
-        context.go("/cheatsheets/view/${sheet.name.replaceAll(" ", "_")}");
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    var isLiked = useState(sheet.starred); // Require this for updating
+
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            autoClose: true,
+            onPressed: (context) async {
+              ref
+                  .read(cheatsheetsRiverpodProvider.notifier)
+                  .toggleCheatsheetLikeStatus(sheet);
+              isLiked.value = !isLiked.value;
+            },
+            backgroundColor: Colors.orangeAccent,
+            foregroundColor: Colors.white,
+            icon: isLiked.value ? Icons.star_border : Icons.star,
+            label: isLiked.value ? 'Unstar' : 'Star',
+          ),
+        ],
+      ),
+      child: ListTile(
+        leading: cheatsheetIcon(sheet.secondaryLevel),
+        title: Text(sheet.name),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            (isLiked.value) ? const Icon(Icons.star) : Container(),
+            const Icon(Icons.chevron_right),
+          ],
+        ),
+        onTap: () {
+          context.go("/cheatsheets/view/${sheet.name.replaceAll(" ", "_")}");
+        },
+      ),
     );
   }
 }
